84fdcbc53870c53adc0623945129e86079b7a60a
luojz <luojz1201@foxmail.com>
2025-06-01 22:22:42 +0800

    style: 布局调整

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,45 @@
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { useState } from 'react'
+import { AppSidebar } from './components/app-sidebar'
+import { Button } from './components/ui/button'
+import { Input } from './components/ui/input'
 import { createTable } from './services/db'
 import { generateId } from './types/data'
-
 export default function App() {
-  function handleCreateTable() {
-    createTable({
-      id: generateId(),
-      name: '我的表格',
-      tables: [],
-    })
+  const [value, setValue] = useState('')
+  async function handleCreateTable() {
+    try {
+      await createTable({
+        id: generateId(),
+        name: value,
+        tables: [],
+      })
+      setValue('')
+    } catch (error) {
+      console.log('error: ', error)
+    }
   }
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold">Notion Table</h1>
-      <button onClick={handleCreateTable}>创建表格</button>
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="flex-1 flex flex-col gap-4">
+        <div className="flex items-center justify-between p-4 gap-4">
+          <SidebarTrigger />
+          <div className="flex-1 flex items-center gap-4">
+            <Input
+              className="max-w-100"
+              placeholder="请输入名称"
+              value={value}
+              onChange={(e) => {
+                setValue(e.target.value)
+              }}
+            />
+            <Button className="max-w-100" onClick={handleCreateTable}>
+              创建表格
+            </Button>
+          </div>
+        </div>
+      </main>
+    </SidebarProvider>
   )
 }
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,48 @@
+import { Home } from 'lucide-react'
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar'
+
+// Menu items.
+const items = [
+  {
+    id: '1',
+    title: '我的表格',
+    url: '#',
+    icon: Home,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Notion Tables</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
